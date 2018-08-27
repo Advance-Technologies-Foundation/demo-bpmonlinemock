5db26a53eb97edadafdc188204acabb37f56db31
alexbondj <alexbondj@gmail.com>
2018-08-27 15:40:20 +0300

    #0 Add project
    Add project

---
+++ BL/CaseStatusStore.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terrasoft.Common;
+using Terrasoft.Core;
+using Terrasoft.Core.Entities;
+using Terrasoft.Core.Store;
+
+namespace BL
+{
+
+	#region Class: CaseStatusStore
+
+	/// <summary>
+	/// Class represents storage of case statuses.
+	/// </summary>
+	public class CaseStatusStore
+	{
+
+		public static readonly Guid CaseStatusReopenedId = new Guid("F063EBBE-FDC6-4982-8431-D8CFA52FEDCF");
+
+		#region Class: CaseStatusData
+
+		/// <summary>
+		/// Class represents case status data from data base.
+		/// </summary>
+		public class CaseStatusData
+		{
+			public Guid Id { get; set; }
+			public string Name { get; set; }
+			public bool IsFinal { get; set; }
+			public bool IsResolved { get; set; }
+			public bool IsPaused { get; set; }
+		}
+
+		#endregion
+
+		#region Constructor: Public
+
+		/// <summary>
+		/// Creates new <see cref="CaseStatusStore"/> instance, using <paramref name="userConnection"/> 
+		/// for loading data from data base.
+		/// </summary>
+		/// <param name="userConnection">User connection <see cref="UserConnection"/> instance.</param>
+		public CaseStatusStore(UserConnection userConnection) {
+			UserConnection = userConnection;
+		}
+
+		/// <summary>
+		/// Creates new <see cref="CaseStatusStore"/> instance, using prepared data store <paramref name="store"/>.
+		/// </summary>
+		/// <param name="store">Prepared data store.</param>
+		public CaseStatusStore(IEnumerable<CaseStatusData> store) {
+			_caseStatusStoreData = store;
+		}
+
+		#endregion
+
+		#region Properties: Private
+
+		private IEnumerable<CaseStatusData> _caseStatusStoreData;
+		private IEnumerable<CaseStatusData> CaseStatusStoreData => _caseStatusStoreData ?? (_caseStatusStoreData = LoadData());
+
+		#endregion
+
+		#region Properties: Protected
+
+		/// <summary>
+		/// Instance of user connection.
+		/// </summary>
+		protected UserConnection UserConnection { get; }
+
+		#endregion
+
+		#region Methods: Private
+
+		private EntitySchemaQuery GetEsqCaseStatus() {
+			var esq = new EntitySchemaQuery(UserConnection.EntitySchemaManager, "CaseStatus");
+			esq.PrimaryQueryColumn.IsAlwaysSelect = true;
+			esq.AddColumn("Name");
+			esq.AddColumn("IsFinal");
+			esq.AddColumn("IsResolved");
+			esq.AddColumn("IsPaused");
+			esq.Cache = UserConnection.SessionCache.WithLocalCaching("CaseStatusStoreCache");
+			esq.CacheItemName = "CaseStatusStoreCacheItem";
+			return esq;
+		}
+
+		#endregion
+
+		#region Methods: Protected
+
+		/// <summary>
+		/// Load case status data from data base.
+		/// </summary>
+		/// <returns>
+		/// Collection case status data.
+		/// </returns>
+		protected IEnumerable<CaseStatusData> LoadData() {
+			var esq = GetEsqCaseStatus();
+			var entities = esq.GetEntityCollection(UserConnection);
+			return entities.Select(entity => new CaseStatusData {
+				Id = entity.GetTypedColumnValue<Guid>(esq.RootSchema.GetPrimaryColumnName()),
+				Name = entity.GetTypedColumnValue<string>("Name"),
+				IsFinal = entity.GetTypedColumnValue<bool>("IsFinal"),
+				IsPaused = entity.GetTypedColumnValue<bool>("IsPaused"),
+				IsResolved = entity.GetTypedColumnValue<bool>("IsResolved")
+			});
+		}
+
+		#endregion
+
+		#region Methods: Public
+
+		/// <summary>
+		/// Check if case status is marked as final. Returns true if it is final, otherwise false.
+		/// </summary>
+		/// <param name="statusId">Uniqueidentifier of case status.</param>
+		/// <returns>
+		/// Checks is case status final.
+		/// </returns>
+		public bool StatusIsFinal(Guid statusId) {
+			return CaseStatusStoreData
+				.Find(status => status.Id == statusId)?.IsFinal ?? false;
+		}
+
+		/// <summary>
+		/// Check if case status is marked as paused. Returns true if it is paused, otherwise false.
+		/// </summary>
+		/// <param name="statusId">Uniqueidentifier of case status.</param>
+		/// <returns>
+		/// Checks is case status paused.
+		/// </returns>
+		public bool StatusIsPaused(Guid statusId) {
+			return CaseStatusStoreData
+				.Find(status => status.Id == statusId)?.IsPaused ?? false;
+		}
+
+		/// <summary>
+		/// Check if case status is marked as resolved. Returns true if it is resolved, otherwise false.
+		/// </summary>
+		/// <param name="statusId">Uniqueidentifier of case status.</param>
+		/// <returns>
+		/// Checks is case status resolved.
+		/// </returns>
+		public bool StatusIsResolved(Guid statusId) {
+			return CaseStatusStoreData
+				.Find(status => status.Id == statusId)?.IsResolved ?? false;
+		}
+
+		/// <summary>
+		/// Check if case status is marked as reopened. Returns true if it is reopened, otherwise false.
+		/// </summary>
+		/// <param name="statusId">Uniqueidentifier of case status.</param>
+		/// <returns>
+		/// Checks is case status reopened.
+		/// </returns>
+		public bool StatusIsReopened(Guid statusId) {
+			return statusId == CaseStatusReopenedId;
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
---
+++ UnitTestProject/CaseStatusStoreTestCase.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BL;
+using FluentAssertions;
+using NUnit.Framework;
+using Terrasoft.Configuration.Tests;
+
+namespace UnitTestProject
+{
+
+	#region Class: CaseBrockerTestCase
+
+	[MockSettings(RequireMock.DBEngine)]
+	[TestFixture]
+	public class CaseStatusStoreTestCase : BaseConfigurationTestFixture
+	{
+
+		private static readonly Guid ClosedStatusId = Guid.Parse("3E7F420C-F46B-1410-FC9A-0050BA5D6C38");
+		private static readonly Guid NewStatusId = Guid.Parse("AE5F2F10-F46B-1410-FD9A-0050BA5D6C38");
+		private static readonly Guid WaitStatusId = Guid.Parse("3859C6E7-CBCB-486B-BA53-77808FE6E593");
+		private static readonly Guid ResolvedStatusId = Guid.Parse("AE7F411E-F46B-1410-009B-0050BA5D6C38");
+
+		private readonly CaseStatusStore _defaultStore = new CaseStatusStore(GetDefaultStore());
+
+
+		public class TestData
+		{
+			public static IEnumerable FinalTestCases {
+				get {
+					yield return new TestCaseData(ClosedStatusId).Returns(true);
+					yield return new TestCaseData(NewStatusId).Returns(false);
+				}
+			}
+			public static IEnumerable PausedTestCases {
+				get {
+					yield return new TestCaseData(ClosedStatusId).Returns(false);
+					yield return new TestCaseData(NewStatusId).Returns(false);
+					yield return new TestCaseData(WaitStatusId).Returns(true);
+				}
+			}
+			public static IEnumerable ResolvedTestCases {
+				get {
+					yield return new TestCaseData(ClosedStatusId).Returns(false);
+					yield return new TestCaseData(NewStatusId).Returns(false);
+					yield return new TestCaseData(ResolvedStatusId).Returns(true);
+				}
+			}
+		}
+
+		#region Methods: Private
+
+		private static IEnumerable<CaseStatusStore.CaseStatusData> GetDefaultStore() {
+			return new List<CaseStatusStore.CaseStatusData> {
+				new CaseStatusStore.CaseStatusData {
+						Id = ClosedStatusId,
+						IsFinal = true,
+						Name = "Closed"
+				}, new CaseStatusStore.CaseStatusData {
+						Id = NewStatusId,
+						Name = "New"
+				}, new CaseStatusStore.CaseStatusData {
+						Id = WaitStatusId,
+						IsPaused = true,
+						Name = "Waiting for response"
+				}, new CaseStatusStore.CaseStatusData {
+						Id = ResolvedStatusId,
+						IsResolved = true,
+						Name = "Resolved"
+				}
+
+			};
+		}
+
+		#endregion
+
+		#region Methods: Protected
+
+		protected override IEnumerable<Type> GetRequiringInitializationSchemas() {
+			EntitySchemaManager.AddCustomizedEntitySchema("CaseStatus", new Dictionary<string, string> {
+				{ "Name", "ShortText" },
+				{ "IsFinal", "Boolean" },
+				{ "IsResolved", "Boolean" },
+				{ "IsPaused", "Boolean" }
+
+			});
+			return base.GetRequiringInitializationSchemas();
+		}
+
+		#endregion
+
+		#region Methods: Public
+		[TestCaseSource(typeof(TestData), nameof(TestData.FinalTestCases))]
+		[Test, Category("PreCommit")]
+		public bool CaseStatusStore_CheckIsStatusFinalById(Guid statusId) {
+			return _defaultStore.StatusIsFinal(statusId);
+		}
+
+		[TestCaseSource(typeof(TestData), nameof(TestData.PausedTestCases))]
+		[Test, Category("PreCommit")]
+		public bool CaseStatusStore_CheckIsStatusPausedByIds(Guid statusId) {
+			return _defaultStore.StatusIsPaused(statusId);
+		}
+
+		[TestCaseSource(typeof(TestData), nameof(TestData.ResolvedTestCases))]
+		[Test, Category("PreCommit")]
+		public bool CaseStatusStore_CheckIsStatusResolvedByIds(Guid statusId) {
+			return _defaultStore.StatusIsResolved(statusId);
+		}
+
+		[TestCaseSource(typeof(TestData), nameof(TestData.ResolvedTestCases))]
+		[Test, Category("PreCommit")]
+		public bool CaseStatusStore_CheckIsStatusResolvedByIds_CallLoadData(Guid statusId) {
+			new SelectData(UserConnection, "CaseStatus")
+				.AddRow(new Dictionary<string, object> { { "Id", ClosedStatusId }, { "Name", "Closed" }, { "IsFinal", true }, { "IsPaused", false }, { "IsResolved", false } })
+				.AddRow(new Dictionary<string, object> { { "Id", NewStatusId }, { "Name", "New" }, { "IsFinal", false }, { "IsPaused", false }, { "IsResolved", false } })
+				.AddRow(new Dictionary<string, object> { { "Id", ResolvedStatusId }, { "Name", "Resolved" }, { "IsFinal", false }, { "IsPaused", false }, { "IsResolved", true } })
+				.MockUp();
+			var store = new CaseStatusStore(UserConnection);
+			return store.StatusIsResolved(statusId);
+		}
+
+		[Test, Category("PreCommit")]
+		public void CaseStatusStore_CheckIsStatusResolvedByIds_StatusIsGuidEmpty() {
+			_defaultStore.StatusIsFinal(Guid.Empty).Should().BeFalse();
+			_defaultStore.StatusIsPaused(Guid.Empty).Should().BeFalse();
+			_defaultStore.StatusIsResolved(Guid.Empty).Should().BeFalse();
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+}
